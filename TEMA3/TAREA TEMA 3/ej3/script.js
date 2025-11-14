b573fe07e78c6a243b420335a6d60ca8ef353850
rodrism3012 <30.rodrigo.12@gmail.com>
2025-11-14 16:35:46 +0100

    refactor cifrado cesar: optimize character substitution logic and improve handling of accented vowels

--- TEMA3/TAREA TEMA 3/ej3/script.js
+++ TEMA3/TAREA TEMA 3/ej3/script.js
@@ -2,41 +2,46 @@
 // reemplaza los caracteres del string al caracter del alfabeto que se encuentre n espacios a la derecha
 // al llegar a la z vuelve a empezar por la a
 function convertirCifradoCesar(cadena, n) {
+  // crear cadenas que funcionen como abecedario
   let alfabetoMayus = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   let alfabetoMinus = "abcdefghijklmnopqrstuvwxyz";
   let vocalesConTildeMayus = "ÁÉÍÓÚ";
   let vocalesConTildeMinus = "áéíóú";
+  // se crea una cadena vacia que sera igual que la original pero despues de haber sido cifrada
   let cadenaCifrada = "";
+  // se puede hacer sustituyendo caracteres en el string original sin crear uno nuevo pero lo encuentro mas sencillo de esta manera
 
-  for (i = 0; i < string.length; i++) {
-    let newChar = ""; // sera la letra por la que se sustituira
-    // en primer lugar comprueba si el caracter es un espacio, y si lo es lo salta y lo deja como esta para ir al sig
-    if (!cadena.charAt(i) == " ") {
-      // busca char en alfabetoMinus; si no lo encuentra pos == false
-      let pos = encontrarCharEnString(alfabetoMinus, cadena.charAt(i));
-      if (pos == false) {
-        // char no se encontro en alfabetoMinus, se va a buscar en alfabetoMayus
-        pos = encontrarCharEnString(alfabetoMayus, cadena.charAt(i));
-        // por el if, solo lo busca si antes no lo encontro
-        if (pos == false) {
-          // si todavia no se encontro, es que hay una vocal con tilde u otro caracter distinto
-          // TODO detectar tildes
-        }
-      } else {
-          cadena.charAt(i) = alfabetoMinus.charAt(pos+n)
-      }
-      // se detectan antes las minusculas porque es mas probable encontrarlas que las mayusculas,
-      // asi al encontrarlas ya no se recorre alfabetoMayus, optimizando el codigo
-
-    }
-  }
-}
-
-// funcion que recorre un string como un array y detecta si en el string se encuentra un caracter concreto (char)
-// al encontrarlo ejecuta un return y se detiene la funcion devolviendo la posicion donde se encontro char (i)
-function encontrarCharEnString(cadena, char) {
+  // bucle para recorrer el string
   for (i = 0; i < cadena.length; i++) {
-    if (cadena.charAt(i) === char) return i;
+    alfabeto = ""; // cadena vacia que se sobreescribe con alguno de los alfabetos dependiendo de las condiciones de los ifs que hay debajo
+    // esto se hace para evitar repetir el codigo de las lineas 34-36 dentro de cada uno de los ifs
+    if (alfabetoMinus.includes(cadena.charAt(i))) {
+      // comprueba si el caracter esta en minuscula
+      alfabeto = alfabetoMinus;
+    } else if (alfabetoMayus.includes(cadena.charAt(i))) {
+      // comprueba si esta en mayuscula
+      alfabeto = alfabetoMayus;
+    } else if (vocalesConTildeMinus.includes(cadena.charAt(i))) {
+      // comprueba si es una vocal con tilde en minuscula
+      alfabeto = vocalesConTildeMinus;
+    } else if (vocalesConTildeMayus.includes(cadena.charAt(i))) {
+      // comprueba si es una vocal con tilde en mayuscula
+      alfabeto = vocalesConTildeMayus;
+    }
+    // los ifs estan ordenados de manera que comprueba los caracteres mas probables de encontrar antes que los mas improbables para optimizar
+    if (alfabeto != "") {
+      // comprueba que alfabeto ha sido sobreescrito despues de entrar en algun if
+      char = cadena.charAt(i); // se saca el caracter segun la posicion dentro del string
+      pos = alfabeto.indexOf(char); // posicion en la que se encuentra el char en el string de alfabeto
+      cadenaCifrada += alfabeto.charAt(pos + n); // se añade al final de la cadena el caracter en el alfabeto con la posicion pos + n
+      // en 1 sola linea seria:
+      // cadenaCifrada += alfabeto.charAt(alfabeto.indexOf(cadena.charAt(i)) + n);
+    } else {
+      // si alfabeto no se ha sobreescrito, es que se trata de un caracter especial y no se cifra, se añade el mismo caracter
+      cadenaCifrada += cadena.charAt(i);
+    }
+    console.log(cadenaCifrada + "\n");
   }
-  return false;
 }
+
+convertirCifradoCesar("hola mundo", 6);
